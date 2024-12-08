c8e0d6b4bfa5e232c1fa90760532d89a4ccf59e5
juniorapeles <elijunior.apeles2005@gmail.com>
2024-12-08 17:05:30 -0300

    first commit

---
+++ src/main/java/com/remedios/backendremedios/BackendRemediosApplication.java
@@ -0,0 +1,13 @@
+package com.remedios.backendremedios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendRemediosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendRemediosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/remedios/backendremedios/BackendRemediosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remedios.backendremedios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendRemediosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
